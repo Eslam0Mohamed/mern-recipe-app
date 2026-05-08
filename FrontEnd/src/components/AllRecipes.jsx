e470ee9361d36d13eb23749226e32754f50fa52f
Eslam Mohamed <eslama202002@gmail.com>
2026-05-08 12:06:12 +0300

    remove .env file

---
+++ FrontEnd/src/components/AllRecipes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AllRecipes = () => {
+  return (
+    <>
+    <section className='bg-[#F9F7F2] py-4'>
+        <div className='container'>
+     <h2 className='text-2xl text-black font-semibold  hover:text-orange-500 w-fit'>All Recipes</h2> 
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default AllRecipes
