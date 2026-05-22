3edb59b7496bc1406463334d4d5fa9a98a104bd8
Eslam Mohamed <eslama202002@gmail.com>
2026-05-22 14:21:37 +0300

    change port number

--- BackEnd/connectDB/connectdb.js
+++ BackEnd/connectDB/connectdb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export function connectDB(){
-    mongoose.connect(process.env.mongoUri)
+    mongoose.connect(process.env.MONGO_URI)
     .then(() => {
       console.log("connected to mongoDB");
     })
--- BackEnd/index.js
+++ BackEnd/index.js
@@ -9,7 +9,7 @@ const app = express()
 dotenv.config()
 connectDB()
 
- const port = process.env.port || 5000
+ const port = process.env.PORT || 5000
 
  app.use(cors())
 
